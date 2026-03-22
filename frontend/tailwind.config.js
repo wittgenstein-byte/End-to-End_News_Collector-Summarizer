6276851ffc4ab91d6b609754050b59fcc52cfd87
Wittensky <nrvsrpl@gmail.com>
2026-03-23 00:14:33 +0700

    add package.json, config, and update deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./frontend/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
